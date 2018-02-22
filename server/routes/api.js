80b9a70f6143826352a092da1c8fefb5ceb48921
Chase Bussey <cbussey52@gmail.com>
2018-02-22 15:55:04 -0600

    Further Mongo URI changes
    Still trying to get Heroku deployment working

--- server/routes/api.js
+++ server/routes/api.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const mongoose = require('mongoose');
 
 // mongo db connection
-mongoose.connect('mongodb://heroku_1v97948b:2gfsjs94iu59qf9t8admgeidd1@ds245548.mlab.com:45548/heroku_1v97948b');
+mongoose.connect(process.env.MONGODB_URI);
 
 // define mongodb model
 var Student = mongoose.model('Student', {
